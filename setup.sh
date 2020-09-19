38fe9f8ba2ca1fef85ee15e13e4df5692bce96a7
Pranita <pranitadeshmukh63@gmail.com>
2020-09-19 19:46:01 +0530

    some changes

--- setup.sh
+++ setup.sh
@@ -3,5 +3,5 @@ export ALGORITHMS=RS256
 export API_AUDIENCE=http://localhost:5000
 export CLINT_ID=gaUn3PN8jut9WYI7PfobQrzSzf6hP1yA
 export FLASK_APP=app.py
-export DATABASE_URL=postgres://xtphweunfhspxm:8a76bf8efa1704d63f3430fcc4b4d64e855c8cd23b5d18d7c11aea7f25d8ae8d@ec2-52-200-134-180.compute-1.amazonaws.com:5432/d98sg1adkdfkha
+export DATABASE_URL=postgres://aerftalqxcofko:44eeb868f078d848bd2e1916025d4a377fb7398068c29e86ba68a5383d9cbec7@ec2-52-200-134-180.compute-1.amazonaws.com:5432/daka35ldpv77ur
 export FLASK_ENV=development
